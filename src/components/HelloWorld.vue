b445337662477c7a7a003508a705c706ac8aa954
xiajun <xiajun@auto11.com>
2020-09-02 10:24:08 +0800

    mui引入到vue项目中

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    created() {
+        //this.mui.alert("你好");
+        this.mui.prompt("", "输入车牌", "请输入车牌");
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped> 
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <ul class="mui-table-view">
+        <li class="mui-table-view-cell">Item 1</li>
+        <li class="mui-table-view-cell">Item 2</li>
+        <li class="mui-table-view-cell">Item 3</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
